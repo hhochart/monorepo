34c564f59f2b3584194d7ea06e82979a8b851429
Hervé <h.hochart29@gmail.com>
2022-03-21 19:16:04 +0100

    feat(tailwind config): update spacing

--- tools/config/tailwind.preset.js
+++ tools/config/tailwind.preset.js
@@ -2,7 +2,43 @@ module.exports = {
   mode: 'jit',
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    spacing: {
+      px: '1px',
+      4: '4px',
+      8: '8px',
+      16: '16px',
+      32: '32px',
+      64: '64px',
+      96: '96px',
+      112: '112px',
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#3C3C3B',
+          dark: '#1F1F1F',
+        },
+        secondary: {
+          DEFAULT: '#986E47',
+          craft: '#C4A181'
+        },
+        neutral: {
+          dark: '#F9F5F2',
+          light: '#F8F8F8'
+        },
+        success: {
+          DEFAULT: '#008315',
+          light: '#F2F9F2'
+        },
+        error: {
+          DEFAULT: '#A9231C',
+          light: '#FCF5F4'
+        }
+      },
+    },
   },
   plugins: [],
 }
